4c36c693f4a78de9241470bf686e382f609d1640
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-21 04:24:42 +0500

    Added a stack for S3 and CloudFront Distribution

---
+++ backend/test/frontend-deploy-stack.test.ts
@@ -0,0 +1,19 @@
+import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as P13cFrontendDeploy from "../lib/frontend-deploy-stack";
+
+test("Stack has a S3 Bucket", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new P13cFrontendDeploy.FrontendDeployStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+});
+
+test("Stack has a CloudFront Distribution", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new P13cFrontendDeploy.FrontendDeployStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::CloudFront::Distribution"));
+});
