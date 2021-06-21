1eb8699a33f87635e9581ea0031967c3dffe26bd
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-22 03:47:23 +0500

    Programatically generate static pages for lollies through createPages API

--- client/src/gatsby/node.ts
+++ client/src/gatsby/node.ts
@@ -1,3 +1,45 @@
 import { GatsbyNode } from "gatsby";
+import { Lolly } from "../graphql/api";
 
-export default {} as GatsbyNode;
+export const createPages: GatsbyNode["createPages"] = async ({
+  graphql,
+  actions,
+}) => {
+  const { data }: QueryResponse = await graphql(`
+    query GetAllLollies {
+      virtualLolly {
+        getAllLollies {
+          id
+          topColor
+          middleColor
+          bottomColor
+          recipientName
+          message
+          sendersName
+        }
+      }
+    }
+  `);
+
+  if (data && data.virtualLolly) {
+    const lollies = data.virtualLolly.getAllLollies;
+    lollies.forEach(lolly => {
+      actions.createPage({
+        path: `/lolly/${lolly.id}`,
+        component: require.resolve("../templates/showLolly"),
+        context: {
+          ...lolly,
+        },
+      });
+    });
+  }
+};
+
+type QueryResponse = {
+  data?: {
+    virtualLolly: {
+      getAllLollies: Lolly[];
+    };
+  };
+  errors?: any;
+};
